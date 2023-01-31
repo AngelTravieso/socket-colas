ba9d5629b8ad1d6e33adfaed925550e2bb2dc802
AngelTravieso <angeltraviesoc@gmail.com>
2023-01-31 19:49:58 -0400

    tickets pendientes por atender

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -3,6 +3,7 @@ const lblEscritorio = document.querySelector('h1');
 const btnAtender = document.querySelector('button');
 const lblTicket = document.querySelector('small');
 const divAlerta = document.querySelector('.alert');
+const lblPendientes = document.querySelector('#lblPendientes');
 
 
 const searchParams = new URLSearchParams(window.location.search);
@@ -19,7 +20,8 @@ const escritorio = searchParams.get('escritorio');
 lblEscritorio.textContent = escritorio;
 
 // Ocultar alerta
-divAlerta.style.dislay = 'none';
+// divAlerta.style.dislay = 'none';
+divAlerta.classList.add('d-none');
 
 
 // * SOCKETS
@@ -36,20 +38,28 @@ socket.on('disconnect', () => {
     btnAtender.disabled = true;
 });
 
-// Escuchar mensaje de ultimo ticket
-socket.on('ultimo-ticket', (ticket) => {
-    // lblNuevoTicket.textContent = ticket;
+// Tickets pendientes
+socket.on('tickets-pendientes', (pendientes) => {
+    // Si no hay tickets pendientes
+    if (pendientes === 0) {
+        lblPendientes.style.display = 'none';
+        divAlerta.classList.remove('d-none');
+    } else {
+        lblPendientes.style.display = '';
+        lblPendientes.textContent = pendientes;
+    }
+
 });
 
 // Siguiente ticket
 btnAtender.addEventListener('click', () => {
 
-    socket.emit('atender-ticket',{ escritorio }, ( { ok, ticket, msg } ) => {
+    socket.emit('atender-ticket', { escritorio }, ({ ok, ticket, msg }) => {
         // Si es false (no hay tickets o hubo algun error)
-        if(!ok) {
+        if (!ok) {
             lblTicket.textContent = 'Nadie.';
             // Mostrar div alerta 'No hay más tickets'
-            return divAlerta.style.display = '';
+            return divAlerta.classList.remove('d-none');
         }
 
         lblTicket.textContent = `Ticket ${ticket.numero}`;
--- sockets/controller.js
+++ sockets/controller.js
@@ -4,22 +4,27 @@ const ticketControl = new TicketControl();
 
 const socketController = (socket) => {
 
+    // Cuando un cliente seconecta
+
     // Emitir evento - ultimo ticket
     socket.emit('ultimo-ticket', `Ticket ${ticketControl.ultimo}`);
 
     // Ultimo 4 tickets
     socket.emit('estado-actual', ticketControl.ultimos4);
 
+    // Tickets pendientes (emit normal)
+    socket.emit('tickets-pendientes', ticketControl.tickets.length);
+
     // Escuchar evento - siguiente ticket
     socket.on('siguiente-ticket', (payload, callback) => {
 
         const siguiente = ticketControl.siguiente();
 
-        console.log(siguiente);
-
+        // Enviar datos siguiente ticket
         callback(siguiente);
 
-        // TODO: Notificar que hay un nuevo ticket pendiente de asignar
+        // Emiti tickets pendientes
+        socket.broadcast.emit('tickets-pendientes', ticketControl.tickets.length);
 
     });
 
@@ -27,7 +32,7 @@ const socketController = (socket) => {
     socket.on('atender-ticket', ({ escritorio }, callback) => {
 
         // Si no llega el nombre del escritorio
-        if(!escritorio) {
+        if (!escritorio) {
             return callback({
                 ok: false,
                 msg: 'El escritorio es obligatorio',
@@ -39,8 +44,14 @@ const socketController = (socket) => {
         // Los ultimos 4 cambiaron y se notifica otra vez ( a las demas pantallas )
         socket.broadcast.emit('estado-actual', ticketControl.ultimos4);
 
+        // Tickets pendientes (broadcast)
+        socket.broadcast.emit('tickets-pendientes', ticketControl.tickets.length);
+
+        // Tickets pendientes (emit normal)
+        socket.emit('tickets-pendientes', ticketControl.tickets.length);
+
         // No hay tickets pendientes
-        if(!ticket) {
+        if (!ticket) {
             callback({
                 ok: false,
                 msg: 'Ya no hay tickets pendientes',
